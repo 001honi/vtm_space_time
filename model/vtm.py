dc2c8a6aee640d62d64e1b6368c983f25decaa6e
Honi <001honi@gmail.com>
2023-09-28 06:00:41 +0000

    Update files from vtm:main_base to ufsl:unified

--- model/vtm.py
+++ model/vtm.py
@@ -1,113 +1,167 @@
+import torch
 import torch.nn as nn
-from .reshape import forward_6d_as_4d, from_6d_to_3d, from_3d_to_6d
-from .attention import CrossAttention
-        
-        
-class VTMImageBackbone(nn.Module):
-    def __init__(self, backbone):
-        super().__init__()
-        self.backbone = backbone
-        
-    def forward(self, x, t_idx=None, **kwargs):
-        return forward_6d_as_4d(self.backbone, x, t_idx=t_idx, get_features=True, **kwargs)
+import random
 
-    def bias_parameters(self):
-        # bias parameters for similarity adaptation
-        for p in self.backbone.bias_parameters():
-            yield p
+from .encoder import ViTEncoder #, get_teachers
+from .decoder import DPTDecoder
+from .matching import MatchingModule
 
-    def bias_parameter_names(self):
-        return [f'backbone.{name}' for name in self.backbone.bias_parameter_names()]
-
-
-class VTMLabelBackbone(nn.Module):
-    def __init__(self, backbone):
-        super().__init__()
-        self.backbone = backbone
-        
-    def encode(self, x, t_idx=None, **kwargs):
-        return forward_6d_as_4d(self.backbone.encode, x, t_idx=t_idx, **kwargs)
-    
-    def decode(self, x, t_idx=None, **kwargs):
-        return forward_6d_as_4d(self.backbone.decode, x, t_idx=t_idx, **kwargs)
-    
-    def forward(self, x, t_idx=None, encode_only=False, decode_only=False, **kwargs):
-        assert not (encode_only and decode_only)
-        if not decode_only:
-            x = self.encode(x, t_idx=t_idx, **kwargs)
-        if not encode_only:
-            x = self.decode(x, t_idx=t_idx, **kwargs)
-        
-        return x
-        
-
-class VTMMatchingModule(nn.Module):
-    def __init__(self, dim_w, dim_z, config):
-        super().__init__()
-        self.matching = nn.ModuleList([CrossAttention(dim_w, dim_z, dim_z, num_heads=config.n_attn_heads)
-                                       for i in range(config.n_levels)])
-        self.n_levels = config.n_levels
-            
-    def forward(self, W_Qs, W_Ss, Z_Ss, attn_mask=None):
-        B, T, N, _, H, W = W_Ss[-1].size()
-        
-        assert len(W_Qs) == self.n_levels
-        
-        if attn_mask is not None:
-            attn_mask = from_6d_to_3d(attn_mask)
-            
-        Z_Qs = []
-        for level in range(self.n_levels):
-            Q = from_6d_to_3d(W_Qs[level])
-            K = from_6d_to_3d(W_Ss[level])
-            V = from_6d_to_3d(Z_Ss[level])
-            
-            O = self.matching[level](Q, K, V, N=N, H=H, mask=attn_mask)
-            Z_Q = from_3d_to_6d(O, B=B, T=T, H=H, W=W)
-            Z_Qs.append(Z_Q)
-        
-        return Z_Qs
+from dataset.unified_dataset import Unified
+from dataset.taskonomy import Taskonomy
 
 
 class VTM(nn.Module):
-    def __init__(self, image_backbone, label_backbone, matching_module):
+    '''
+    Visual Token Matching
+    '''
+    def __init__(self, config, n_tasks):
         super().__init__()
-        self.image_backbone = image_backbone
-        self.label_backbone = label_backbone
-        self.matching_module = matching_module
-        
-        self.n_levels = self.image_backbone.backbone.n_levels
+        self.image_encoder = ViTEncoder(config.image_encoder, pretrained=(config.stage == 0 and
+                                                                          not (config.continue_mode or
+                                                                               config.resolution_finetune_mode)),
+                                        in_chans=3, n_bias_sets=n_tasks, n_levels=config.n_levels,
+                                        drop_path_rate=config.image_encoder_drop_path_rate,
+                                        qkv_bitfit=getattr(config, 'qkv_bitfit', True),
+                                        additional_bias=getattr(config, 'additional_bias', False),
+                                        time_attn=config.time_attn)
+        self.label_encoder = ViTEncoder(config.label_encoder, pretrained=False, in_chans=1, n_bias_sets=0,
+                                        n_levels=config.n_levels,
+                                        drop_path_rate=config.label_encoder_drop_path_rate,
+                                        time_attn=config.time_attn)
+        self.matching_module = MatchingModule(self.image_encoder.backbone.embed_dim,
+                                              self.label_encoder.backbone.embed_dim,
+                                              config.n_attn_heads, n_levels=config.n_levels)
+        self.label_decoder = DPTDecoder(self.label_encoder.grid_size, self.label_encoder.backbone.embed_dim,
+                                        hidden_features=[min(config.decoder_features*(2**i), 1024)
+                                                         if config.n_levels == 4
+                                                         else min(config.decoder_features*(2**(i//2)), 1024)
+                                                         for i in range(config.n_levels)],
+                                        deconv_head=config.deconv_head)
 
     def bias_parameters(self):
         # bias parameters for similarity adaptation
-        for p in self.image_backbone.bias_parameters():
+        for p in self.image_encoder.bias_parameters():
             yield p
 
     def bias_parameter_names(self):
-        return [f'image_backbone.{name}' for name in self.image_backbone.bias_parameter_names()]
+        names = [f'image_encoder.{name}' for name in self.image_encoder.bias_parameter_names()]
+
+        return names
+
+    def additional_bias_parameters(self):
+        # bias parameters for similarity adaptation
+        for p in self.image_encoder.additional_bias_parameters():
+            yield p
+
+    def additional_bias_parameter_names(self):
+        names = [f'image_encoder.{name}' for name in self.image_encoder.additional_bias_parameter_names()]
+
+        return names
+
+    def head_parameters(self):
+        return self.label_decoder.head.parameters()
 
     def pretrained_parameters(self):
-        return self.image_backbone.parameters()
+        return self.image_encoder.parameters()
     
     def scratch_parameters(self):
-        modules = [self.label_backbone, self.matching_module]
+        modules = [self.label_encoder, self.matching_module, self.label_decoder]
         for module in modules:
             for p in module.parameters():
                 yield p
-        
-    def forward(self, X_S, Y_S, X_Q, t_idx=None, sigmoid=True):
-        # encode support input, query input, and support output
-        W_Ss = self.image_backbone(X_S, t_idx)
-        W_Qs = self.image_backbone(X_Q, t_idx)
-        Z_Ss = self.label_backbone.encode(Y_S, t_idx)
-        
+
+    def forward(self, X_S, Y_S, X_Q, t_idx=None):
+
+        # encode query input, support input and output
+        if isinstance(X_S, tuple):
+            X_S1, X_S2 = X_S
+            X_Q1, X_Q2 = X_Q
+            W_Qs = self.image_encoder(X_Q1, t_idx), self.image_encoder(X_Q2, t_idx)
+            W_Ss = self.image_encoder(X_S1, t_idx), self.image_encoder(X_S2, t_idx)
+        else:
+            W_Qs = self.image_encoder(X_Q, t_idx)
+            W_Ss = self.image_encoder(X_S, t_idx)
+
+        Z_Ss = self.label_encoder(Y_S)
+
         # mix support output by matching
         Z_Q_preds = self.matching_module(W_Qs, W_Ss, Z_Ss)
         
         # decode support output
-        Y_Q_pred = self.label_backbone.decode(Z_Q_preds, t_idx)
-        
-        if sigmoid:
-            Y_Q_pred = Y_Q_pred.sigmoid()
+        Y_Q_pred = self.label_decoder(Z_Q_preds)
         
         return Y_Q_pred
+
+
+class DPT(nn.Module):
+    '''
+    Dense Prediction Transformer
+    '''
+    def __init__(self, config, n_tasks):
+        super().__init__()
+        self.image_encoder = ViTEncoder(config.image_encoder, pretrained=(config.stage == 0 and
+                                                                          not (config.continue_mode or
+                                                                               config.resolution_finetune_mode)),
+                                        n_bias_sets=0,
+                                        in_chans=3,
+                                        drop_path_rate=config.image_encoder_drop_path_rate)
+        self.label_decoder = DPTDecoder(self.image_encoder.grid_size, self.image_encoder.backbone.embed_dim,
+                                        hidden_features=[min(config.decoder_features*n, 1024) for n in range(1, 5)],
+                                        deconv_head=config.deconv_head,
+                                        out_chans=n_tasks)
+
+    def pretrained_parameters(self):
+        return self.image_encoder.parameters()
+
+    def scratch_parameters(self):
+        return self.label_decoder.parameters()
+
+    def forward(self, X):
+        # encode
+        Zs = self.image_encoder(X[None, None])
+
+        # cut off cls token
+        Zs = [Z[:, :, :, 1:] for Z in Zs]
+
+        # decode
+        Y_pred = self.label_decoder(Zs)[0, 0]
+        
+        return Y_pred
+
+    
+
+def get_model(config, verbose=False):
+
+    # set number of tasks for bitfit
+    if getattr(config, 'bitfit', True):
+        if config.stage == 0 and config.model == 'VTM':
+            n_tasks = len(Unified.TASKS)
+        else:
+            if config.channel_idx < 0:
+                if config.dataset == 'taskonomy':
+                    n_tasks = len(Taskonomy.TASK_GROUP_DICT[config.task])
+                elif config.task == 'pose_6d':
+                    n_tasks = 9
+                elif config.task == 'flow':
+                    n_tasks = 2
+                elif config.task == 'derain':
+                    n_tasks = 3
+                elif config.task == 'semseg':
+                    n_tasks = 7
+                elif config.task == 'animalkp':
+                    n_tasks = 17    
+                else:
+                    n_tasks = 1
+            else:
+                n_tasks = 1
+    else:
+        n_tasks = 0
+
+    if config.model == 'VTM':
+        model = VTM(config, n_tasks)
+        if verbose:
+            print(f'Registered VTM with {n_tasks} task-specific bias parameters.')
+    elif config.model == 'DPT':
+        model = DPT(config, n_tasks)
+
+    return model
